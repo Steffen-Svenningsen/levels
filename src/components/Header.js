abadf1fc336bb5b1534442e7374edea48d03a2a3
Tomme-git <tommehansen@gmail.com>
2023-04-11 22:06:34 +0200

    bandaid fix for footer icon, hamburger menu + animations
    Footer icon inherits header nav styling when the header nav gets set to display: none for whatever reason, so another icon with unique ids on the svg has been created for the footer (bandaid fix).
    
    comments explain the hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { NavLink } from 'react-router-dom'
 import Logo from './icons/Logo'
 import MailIcon from './icons/MailIcon'
@@ -6,31 +6,65 @@ import FacebookIcon from './icons/FacebookIcon'
 import InstagramIcon from './icons/InstagramIcon'
 
 export default function Header() {
+
+  const ref = useRef(null);
+
+  // hamburger line state
+  const [open, setOpen] = useState();
+
+  // check if hamburger menu contains inactive class on mount
+  // done to prevent animation from playing on page load - just in case :)
+  useEffect(() => {
+    if (ref.current.classList.contains('inactive')) {
+      ref.current.classList.remove('inactive');
+      console.log('Element contained class and was removed for page load');
+    } else {
+      console.log('Element did not contain inactive class, so we are good');
+    }
+  }, []);
+
+  // handle click event
+  const HandleClick = e => {
+    console.log('clicked');
+    if (ref.current.classList.contains('active')) {
+      ref.current.classList.remove('active');
+      ref.current.classList.add('inactive');
+      console.log('classlist contained active class, so it was replaced with inactive class')
+    } else {
+      ref.current.classList.add('active');
+      console.log('active class was added')
+    }
+
+    // flips the boolean value on open when function is called
+    setOpen(!open);
+    console.log('Boolean for open was flipped')
+  };
+
   return (
     <header>
-        <NavLink className="logo" to="/"><Logo/>Levels</NavLink>
-        <nav>
-            <NavLink to="/ydelser">Ydelser</NavLink>
-            <NavLink to="/cases">Cases</NavLink>
-            <NavLink to="/priser">Priser</NavLink>
-            <NavLink to="/om-os">Om os</NavLink>
-            <NavLink to="/vaerdier">Værdier</NavLink>
-            <div className='header-seperator-line'></div>
-            <a href='#' target='_blank'><FacebookIcon/></a>
-            <a href='#' target='_blank'><InstagramIcon/></a>
-            <a href='mailto:info@levels.com' target='_blank'><MailIcon/></a>
-        </nav>
-        <div className='hamburger'>
-            <div className='hamburger-line'></div>
-            <div className='hamburger-line'></div>
-        </div>
-        <div className='mobile-menu'>
-            <NavLink to="/ydelser">Ydelser</NavLink>
-            <NavLink to="/cases">Cases</NavLink>
-            <NavLink to="/priser">Priser</NavLink>
-            <NavLink to="/om-os">Om os</NavLink>
-            <NavLink to="/vaerdier">Værdier</NavLink>
-        </div>
+      <NavLink className="logo" to="/"><Logo />Levels</NavLink>
+      <nav className='desktop-menu'>
+        <NavLink to="/ydelser">Ydelser</NavLink>
+        <NavLink to="/cases">Cases</NavLink>
+        <NavLink to="/priser">Priser</NavLink>
+        <NavLink to="/om-os">Om os</NavLink>
+        <NavLink to="/vaerdier">Værdier</NavLink>
+        <div className='header-seperator-line'></div>
+        <a href='#' target='_blank'><FacebookIcon /></a>
+        <a href='#' target='_blank'><InstagramIcon /></a>
+        <a href='mailto:info@levels.com' target='_blank'><MailIcon /></a>
+      </nav>
+      <div className='hamburger' onClick={HandleClick}>
+        <div className='hamburger-line'></div>
+        <div className={`hamburger-line ${open ? 'smaller-line' : ''}`}></div> {/* add class or empty string based on open value */}
+      </div>
+      <nav ref={ref} className={`mobile-menu`}>
+        <NavLink onClick={HandleClick} to="/ydelser">Ydelser</NavLink>
+        <NavLink onClick={HandleClick} to="/cases">Cases</NavLink>
+        <NavLink onClick={HandleClick} to="/priser">Priser</NavLink>
+        <NavLink onClick={HandleClick} to="/om-os">Om os</NavLink>
+        <NavLink onClick={HandleClick} to="/vaerdier">Værdier</NavLink>
+      </nav>
     </header>
   )
 }
