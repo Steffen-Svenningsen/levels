0909159c23109e7d7d5c18273c723b340a20f82e
Steffen-Svenningsen <steffen@zynaps.dk>
2023-04-13 15:49:25 +0200

    Add text-component to hero-section

--- src/pages/Cases.js
+++ src/pages/Cases.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import SubpageHeroText from '../components/SubpageHeroText'
 
 export default function Cases() {
   return (
-    <div className='page'>Cases</div>
+    <div className='page'>
+      <SubpageHeroText whiteText="Hos Levels er vi stolte " greyText="over at kunne vise vores arbejde for vores kunder frem."/>
+    </div>
   )
 }
--- src/pages/Values.js
+++ src/pages/Values.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import SubpageHeroText from '../components/SubpageHeroText'
 
 export default function Values() {
   return (
-    <div className='page'>Values</div>
+    <div className='page'>
+      <SubpageHeroText whiteText="Vi tror på at vi sammen kan skabe en grønnere fremtid. " greyText="Men vi har brug for din hjælp."/>
+    </div>
   )
 }
