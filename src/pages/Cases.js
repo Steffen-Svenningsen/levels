d1657322fbc337c305a5e16c82c5e68a65bca637
Steffen-Svenningsen <steffen@zynaps.dk>
2023-04-14 15:56:19 +0200

    Add minimal content

--- src/pages/Cases.js
+++ src/pages/Cases.js
@@ -5,6 +5,13 @@ export default function Cases() {
   return (
     <div className='page'>
       <SubpageHeroText whiteText="Hos Levels er vi stolte " greyText="over at kunne vise vores arbejde for vores kunder frem."/>
+      <section>
+        <h1>Seneste cases.</h1>
+        <div className='gradient-line'></div>
+      </section>
+      <section className='quote-section'>
+        <h1 className='shade-center'>Vi har gennem mere end 10 år tilfredstillet et hav af kunder indenfor webdesign og webudvikling. <span className='span-white'>Er du den næste i rækken?</span></h1>
+      </section>
     </div>
   )
 }
